2ef469a401334b0681b4381d60ec189312921f35
Cfcool100 ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-07-04 21:48:58 +0000

    ...

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,22 @@
 module.exports = {
+  // Indique les chemins des fichiers de test
+  testMatch: [
+    '**/__tests__/**/*.js',
+    '**/?(*.)+(spec|test).js'
+  ],
+
   preset: "@vue/cli-plugin-unit-jest",
-  transformIgnorePatterns: ["node_modules/(?!axios)"],
   
+  // Spécifie les dossiers à exclure des tests
+  testPathIgnorePatterns: [
+    '/node_modules/(?!axios)'
+  ],
+  
+  // Définit un module de transpilation si nécessaire (par exemple, Babel)
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  }
+  
+  // Ajoutez d'autres options de configuration selon vos besoins
 };
 
