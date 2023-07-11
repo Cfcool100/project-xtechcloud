3dec510b4fb3e40ca20319cc6402d3390e1e22f6
Cfcool100 ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-07-11 23:45:33 +0000

    homePage mobile finished

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../img/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faClose } from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const HandleClick = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div className='Navbar'>
-      <div className='logo'>  
-      <Link to={'/'}>
-        <img src={logo} alt="" />
-      </Link>
+      <div className='logo'>
+        <Link to={'/'}>
+          <img src={logo} alt="" />
+        </Link>
       </div>
       <div className='nav'>
-      <Link to={'/'}>Accueil</Link>
-      <Link to={'/services'}>Services</Link>
-      <Link to={'/blog'}>Blog</Link>
-      <Link to={'/contact'}>Contact</Link>
-      <FontAwesomeIcon icon={faBars} size="lg" style={{color: "#f1f4f8",}} />
+        <Link to={'/'}>Accueil</Link>
+        <Link to={'/services'}>Services</Link>
+        <Link to={'/blog'}>Blog</Link>
+        <Link to={'/contact'}>Contact</Link>
+        <FontAwesomeIcon
+          icon={isOpen ? faClose : faBars}
+          size='lg'
+          style={{ color: '#f1f4f8' }}
+          onClick={HandleClick}
+        />
       </div>
     </div>
   )
